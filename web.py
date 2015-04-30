8d7d55bcc8c75960d6cc0b43d8e0dbc14c7cd793
Anton Jansson <anton_jansson@live.se>
2015-04-30 12:15:35 +0200

    Processing pipeline of the different stages.

--- web.py
+++ web.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 import config
+import crawler
+import gavagai
+import processor
 
 # Initializing the web app
 app = Flask(__name__)
@@ -9,6 +12,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Actions
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query') or ''
+    for result in processor.process(query):
+        print result.title
+        for score in result.scores:
+            print "\t{}: score: {}, normalized score: {}".format(score.tone, score.score, score.normalizedScore)
+    return ''
 
 if __name__ == '__main__':
     app.run(host=config.flask_host, port=config.flask_port, debug=config.flask_debug)
