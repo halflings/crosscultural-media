206054d770139cae8c53fe976347702d4357cbd1
Anton Jansson <anton_jansson@live.se>
2015-05-03 13:27:05 +0200

    Integrated the context extraction function to the processor.

--- processor.py
+++ processor.py
@@ -2,6 +2,8 @@ import config
 import gavagai
 import newspaper
 import crawler
+import extractor
+from json import JSONEncoder
 
 #Score for a tone
 class Score(object):
@@ -10,36 +12,51 @@ class Score(object):
         self.score = score
         self.normalizedScore = normalizedScore
 
+#Holds results for a sentence
+class Sentence(object):
+    def __init__(self, num, scores):
+        self.num = num
+        self.scores = scores
+
 #Holds results
 class Result(object):
-    def __init__(self, title, scores):
+    def __init__(self, title, sentences):
         self.title = title
-        self.scores = scores
+        self.sentences = sentences
+
+#JSON serializer
+class ResultEncoder(JSONEncoder):
+    def default(self, data):
+        return data.__dict__
 
 #Process the given query
 def process(query):
     articles = crawler.fetch_articles(query, 'en')
-    return processGavagai(query, articles)
+    return process_gavagai(query, articles)
 
 #Process the given articles
-def processGavagai(query, articles):
+def process_gavagai(query, articles):
     api = gavagai.Gavagai(config.GAVAGAI_API_KEY)
     texts = []
-    titles = {}
+    results = []
+    sentences = {}
 
     #Create the request object
-    id = 0
+    articleId = 0
+    sentenceId = 0
     for article in articles:
-        texts.append(dict(body=extractSentences(query, article), id=str(id)))
-        titles[str(id)] = article.title
-        id += 1
+        for sentence in extract_sentences(article, query):
+            texts.append(dict(body=sentence, id=str(sentenceId)))
+            sentences[str(sentenceId)] = articleId
+            sentenceId += 1
+
+        results.append(Result(article.title, []))
+        articleId += 1
 
     #Get the tonality score
     response = api.tonality(texts, "en")
 
     #Create the results
-    results = []
-
     for text in texts:
         scores = []
         for resp in response['texts']:
@@ -47,11 +64,12 @@ def processGavagai(query, articles):
                 break
         for tone in sorted(resp['tonality'], key=lambda t : t['score'], reverse=True):
             scores.append(Score(tone['tone'], tone['score'], tone['normalizedScore']))
-        title = titles[text['id']]
-        results.append(Result(title, scores))
+        result = results[sentences[text['id']]]
+        result.sentences.append(Sentence(len(result.sentences), scores))
 
     return results
 
 #Extract the important sentences from the given article
-def extractSentences(query, article):
-    return article.text
+def extract_sentences(article, query):
+    #return [article.text]
+    return extractor.extract_surrounded_context(article.text, query)
