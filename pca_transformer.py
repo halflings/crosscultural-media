f833ea8a16abbe90d37ee181729ba8d5fa94af80
Anton Jansson <anton_jansson@live.se>
2015-05-10 14:32:18 +0200

    Added searching and displaying the results in the browser.
    This includes searching for a query, processing it and projecting the
    result via a PCA. The PCA result is not atm stored in the DB.

---
+++ pca_transformer.py
@@ -0,0 +1,54 @@
+from itertools import cycle
+
+import mongoengine
+import matplotlib.pyplot as plt
+import sklearn.decomposition
+
+import config
+from crawler import enqueue_query, process_query
+
+# Converts the 'array' type returned from pca.transform to a Python array
+def toArray(projection):
+    data = []
+    for projPoint in projection:
+        point = []
+        for comp in projPoint:
+            point.append(comp)
+        data.append(point)
+    return data
+
+# Projects data points for all languages in the given query
+def project(query_job):
+    pca = sklearn.decomposition.PCA(2)
+
+    # Getting all datapoints to learn the PCA / project individual articles
+    total_datapoints = []
+    datapoints_by_language = dict()
+    for query in query_job.queries:
+        datapoints = [a.score_vector for a in query.articles]
+        total_datapoints += datapoints
+        datapoints_by_language[query.language] = datapoints
+
+    # Fitting the PCA
+    pca.fit(total_datapoints)
+
+    # Projecting articles by language
+    results = []
+    for query in query_job.queries:
+        datapoints = datapoints_by_language[query.language]
+        projection = pca.transform(datapoints)
+        results.append(dict(query=query.text, language=query.language, results=toArray(projection)))
+
+    return results
+
+def get_results(query, language):
+    db = mongoengine.connect(config.db_name)
+    db.drop_database(config.db_name)
+
+    query_job = enqueue_query(query, language)
+    process_query(query_job)
+
+    return project(query_job)
+
+if __name__ == '__main__':
+    print get_results('coffee', 'en')
--- web.py
+++ web.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, jsonify
 
 import config
 import json
+import mongoengine
+import pca_transformer
+from crawler import processing_routine, enqueue_query
+from model import QueryJob
 
 # Initializing the web app
 app = Flask(__name__)
@@ -14,14 +18,29 @@ def index():
 # Actions
 @app.route('/search', methods=['GET'])
 def search():
-    query = request.args.get('query') or ''
-    resEnglish = dict(query='coffe', language='en', results=[[-0.5, 0.2], [-0.15, -0.1], [0.25, -0.35], [0.6 ,-0.05], [0.7, 0], [0.9, 0.4]])
-    resFrench = dict(query='cafe', language='fr', results=[[-0.5, 0.2], [-0.41, 0.05], [-0.25, -0.25], [-0.05, 0.1], [0.01, -0.4], [0.07, -0.02], [0.25, -0.3], [0.35, 0.3]])
-    return json.dumps([resEnglish, resFrench])
+	query = request.args.get('query') or ''
+	#resEnglish = dict(query='coffee', language='en', results=[[-0.61748019,  0.10154602], [-0.61748019,  0.10154602], [ 0.51323593, -0.03923523], [ 0.60949273,  0.14345396], [-0.61748019,  0.10154602], [-0.47234277, -0.00079441], [-0.61748019,  0.10154602], [ 0.2137013, -0.44803087], [ 0.76519622,  0.38417246], [ 0.76519622,  0.38417246]])
+	#resSwedish = dict(query='kaffe', language='sv', results=[[ 0.1815497,  -0.21035415], [ 0.3043041,   0.28996368], [-0.61748019,  0.10154602], [ 0.13368673, -0.00892597], [ 0.6372332,   0.16000524], [ 0.2137013,  -0.44803087], [-0.61748019,  0.10154602], [ 0.2137013,  -0.44803087], [ 0.2137013,  -0.44803087], [-0.58747614,  0.08038933]])
+	#return json.dumps(dict(query='coffee', results=[resEnglish, resSwedish]))
+	lang = "en"
+	mongoengine.connect(config.db_name)
+	jobs = QueryJob.objects(text=query, language=lang, processed=True)
+	query_job = None
+
+	# Check if any jobs exists
+	if len(jobs) == 0:
+		# Else process it
+		enqueue_query(query, lang)
+		processing_routine()
+		query_job = QueryJob.objects(text=query, language=lang, processed=True)[0]
+	else:
+		query_job = jobs[0]
+
+	return json.dumps(dict(query=query, results=pca_transformer.project(query_job)))
 
 @app.route('/results', methods=['GET'])
 def result():
-    return render_template('result.html')
+	return render_template('result.html', query=request.args.get('query', ''))
 
 if __name__ == '__main__':
     app.run(host=config.flask_host, port=config.flask_port, debug=config.flask_debug)
