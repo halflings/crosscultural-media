d6360095be0299a6995f293b6b304b7cdd893d49
Anton Jansson <anton_jansson@live.se>
2015-05-10 12:25:50 +0200

    Started working on display the result.

--- web.py
+++ web.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 
 import config
-import crawler
-import gavagai
-import processor
-from processor import Result, Sentence, Score, ResultEncoder
 import json
 
 # Initializing the web app
@@ -19,15 +15,13 @@ def index():
 @app.route('/search', methods=['GET'])
 def search():
     query = request.args.get('query') or ''
-    results = processor.process(query)
-    for result in results:
-        print result.title
-        for sentence in result.sentences:
-            print "sentence #{}".format(sentence.num)
-            for score in sentence.scores:
-                print "\t{}: score: {}, normalized score: {}".format(score.tone, score.score, score.normalizedScore)
-    #return json.dumps(results, cls=ResultEncoder, sort_keys=True, indent=4, separators=(',', ': '))
-    return ''
+    resEnglish = dict(query='coffe', language='en', results=[[-0.5, 0.2], [-0.15, -0.1], [0.25, -0.35], [0.6 ,-0.05], [0.7, 0], [0.9, 0.4]])
+    resFrench = dict(query='cafe', language='fr', results=[[-0.5, 0.2], [-0.41, 0.05], [-0.25, -0.25], [-0.05, 0.1], [0.01, -0.4], [0.07, -0.02], [0.25, -0.3], [0.35, 0.3]])
+    return json.dumps([resEnglish, resFrench])
+
+@app.route('/results', methods=['GET'])
+def result():
+    return render_template('result.html')
 
 if __name__ == '__main__':
     app.run(host=config.flask_host, port=config.flask_port, debug=config.flask_debug)
